9ff437ae3d54f59c0c4c6a8f8929dd3208cf710d
Ankur <vasavada.ankur@gmail.com>
2020-04-16 19:48:35 -0400

    created service and Get method

--- LastOfUs/src/app/services/tmapi.service.ts
+++ LastOfUs/src/app/services/tmapi.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TMapiService {
+  baseUrl = 'https://app.ticketmaster.com/discovery/v2/events';
+  API_KEY = '.json?apikey=zMf7gfbAyAigJLCB0a1iMrDv6OK8IDz9';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getEvents() {
+    return this.http.get(`${this.baseUrl}${this.API_KEY}`);
+  }
 }
