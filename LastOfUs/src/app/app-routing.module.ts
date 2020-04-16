5f27fb0e70a0adaa9bb1bdfa1b9b06e910773cd0
Ankur <vasavada.ankur@gmail.com>
2020-04-16 19:34:04 -0400

    imported http module

--- LastOfUs/src/app/app-routing.module.ts
+++ LastOfUs/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventsComponent } from './components/events/events.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: EventsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
