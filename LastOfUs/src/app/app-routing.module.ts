174f306ed3a4a1e45b5ef846685ffe65018befcf
Ankur <vasavada.ankur@gmail.com>
2020-04-16 21:55:06 -0400

    Working on details

--- LastOfUs/src/app/app-routing.module.ts
+++ LastOfUs/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EventsComponent } from './components/events/events.component';
+import { DetailsComponent } from './components/details/details.component';
 
-const routes: Routes = [{ path: '', component: EventsComponent }];
+const routes: Routes = [
+  { path: '', component: EventsComponent },
+
+  { path: 'details/:eventId', component: DetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
