7b603069b1df6ad6b8fcec3ad11375c879cf3c0a
Ankur <vasavada.ankur@gmail.com>
2020-04-16 20:07:11 -0400

    Getting data in events module

--- LastOfUs/src/app/components/events/events.component.ts
+++ LastOfUs/src/app/components/events/events.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { TMapiService } from 'src/app/services/tmapi.service';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.css']
+  styleUrls: ['./events.component.css'],
 })
 export class EventsComponent implements OnInit {
+  events;
 
-  constructor() { }
+  constructor(private tmAPI: TMapiService) {}
 
   ngOnInit(): void {
+    this.tmAPI.getEvents().subscribe((data) => {
+      // console.log(data);
+      this.events = data['_embedded'].events;
+      console.log(this.events);
+    });
   }
-
 }
