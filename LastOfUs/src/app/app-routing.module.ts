ae187e6b03503ff9d3f151d36b749463da566933
Ankur <vasavada.ankur@gmail.com>
2020-04-20 21:27:05 -0400

    little bug

--- LastOfUs/src/app/app-routing.module.ts
+++ LastOfUs/src/app/app-routing.module.ts
@@ -8,8 +8,6 @@ const routes: Routes = [
   { path: '', component: EventsComponent },
   { path: 'details/:eventID', component: DetailsComponent },
   { path: 'bucket-list', component: BucketListComponent },
-];
-
   { path: 'details/:eventId', component: DetailsComponent },
 ];
 
