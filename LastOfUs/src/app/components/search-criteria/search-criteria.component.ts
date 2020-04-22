34e061cc2f77930c42fb9da806999878eb03cf0d
Ankur <vasavada.ankur@gmail.com>
2020-04-22 16:59:38 -0400

    created genre drop downs

--- LastOfUs/src/app/components/search-criteria/search-criteria.component.ts
+++ LastOfUs/src/app/components/search-criteria/search-criteria.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { TMapiService } from '../../services/tmapi.service';
+import { Categories } from 'src/app/interfaces/categories';
 
 @Component({
-  selector: 'app-search-criteria',
+  selector: 'search-criteria',
   templateUrl: './search-criteria.component.html',
-  styleUrls: ['./search-criteria.component.css']
+  styleUrls: ['./search-criteria.component.css'],
 })
 export class SearchCriteriaComponent implements OnInit {
+  misc: Categories[];
+  sports: Categories[];
+  music: Categories[];
+  artsTheatre: Categories[];
+  films: Categories[];
 
-  constructor() { }
+  constructor(private api: TMapiService) {}
 
   ngOnInit(): void {
+    this.api.getClassifications().subscribe((data) => {
+      const genresArray = data['_embedded'].classifications
+        .filter((x) => x.segment)
+        .map((x) => {
+          const { _embedded } = x.segment;
+          return _embedded.genres;
+        });
+      this.misc = genresArray[0];
+      this.sports = genresArray[1];
+      this.music = genresArray[2];
+      this.artsTheatre = genresArray[3];
+      this.films = genresArray[5];
+    });
   }
 
+  searchKeywords(x) {
+    console.log(x);
+  }
+
+  optionValue(x) {
+    console.log(x);
+  }
 }
